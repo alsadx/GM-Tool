ed85ce5210d16291ea0bf5630fbf858b5782c8cd
Sadykov <ortem622@gmail.com>
2025-05-15 15:56:06 +0500

    refactoring ability & skill, testing

--- character-service/pkg/domain/character/character.go
+++ character-service/pkg/domain/character/character.go
@@ -1,7 +1,24 @@
 package character
 
+import (
+	"github.com/alsadx/GM-Tool/character-service/pkg/domain/ability"
+	"github.com/alsadx/GM-Tool/character-service/pkg/domain/armor"
+	"github.com/alsadx/GM-Tool/character-service/pkg/domain/health"
+	"github.com/alsadx/GM-Tool/character-service/pkg/domain/level"
+	"github.com/alsadx/GM-Tool/character-service/pkg/domain/types"
+)
+
 type Character struct {
 	ID    int
-	Name  string
 	Owner int
+
+	Name  string
+	Class string
+	Race  string
+	Lvl   *level.LevelSystem
+	Armor *armor.Armor
+
+	Stats map[types.AbilityType][]ability.Ability
+
+	Health *health.HealthPoint
 }
