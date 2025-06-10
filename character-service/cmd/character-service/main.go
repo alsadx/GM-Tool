4777035778d9fa6d73858f4ddf67d19e3e3150d5
Sadykov <ortem622@gmail.com>
2025-06-10 14:42:03 +0500

    stats service integration

--- character-service/cmd/character-service/main.go
+++ character-service/cmd/character-service/main.go
@@ -9,12 +9,15 @@ import (
 	character_core "github.com/alsadx/GM-Tool/character-service/gen/service/character-core"
 	character_health "github.com/alsadx/GM-Tool/character-service/gen/service/character-health"
 	character_progress "github.com/alsadx/GM-Tool/character-service/gen/service/character-progress"
+	character_stats "github.com/alsadx/GM-Tool/character-service/gen/service/character-stats"
 	core_controller "github.com/alsadx/GM-Tool/character-service/internal/controller/character-core"
 	health_controller "github.com/alsadx/GM-Tool/character-service/internal/controller/character-health"
 	progress_controller "github.com/alsadx/GM-Tool/character-service/internal/controller/character-progress"
+	stats_controller "github.com/alsadx/GM-Tool/character-service/internal/controller/character-stats"
 	handler_core "github.com/alsadx/GM-Tool/character-service/internal/handlers/character-core"
 	handler_health "github.com/alsadx/GM-Tool/character-service/internal/handlers/character-health"
 	handler_progress "github.com/alsadx/GM-Tool/character-service/internal/handlers/character-progress"
+	handler_stats "github.com/alsadx/GM-Tool/character-service/internal/handlers/character-stats"
 	mongoRepo "github.com/alsadx/GM-Tool/character-service/internal/repository/mongo"
 	"github.com/go-logr/zapr"
 	"go.uber.org/zap"
@@ -71,10 +74,12 @@ func main() {
 	ctrl_core := core_controller.New(repo)
 	ctrl_health := health_controller.New(repo)
 	ctrl_progress := progress_controller.New(repo)
+	ctrl_stats := stats_controller.New(repo)
 
 	handler_core := handler_core.New(ctrl_core)
 	handler_health := handler_health.New(ctrl_health)
 	handler_progress := handler_progress.New(ctrl_progress)
+	handler_stats := handler_stats.New(ctrl_stats)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
@@ -86,6 +91,7 @@ func main() {
 	character_core.RegisterCharacterCoreServiceServer(srv, handler_core)
 	character_health.RegisterCharacterHealthServiceServer(srv, handler_health)
 	character_progress.RegisterCharacterProgressServiceServer(srv, handler_progress)
+	character_stats.RegisterCharacterStatsServiceServer(srv, handler_stats)
 
 	if err := srv.Serve(lis); err != nil {
 		panic(err)
